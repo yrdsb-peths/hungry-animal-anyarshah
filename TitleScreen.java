4513dbe3f3c278f9c008b202c0eaac16aca4b1da
anyarshah <348612037@gapps.yrdsb.ca>
2022-12-12 12:39:59 -0500

    Added instructions

--- TitleScreen.java
+++ TitleScreen.java
@@ -17,9 +17,10 @@ public class TitleScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        addObject(titleLabel, getWidth()/2, getHeight()/2);
+        addObject(titleLabel, 245, 50);
+        prepare();
     }
-    
+
     public void act()
     {
         if(Greenfoot.isKeyDown("space"))
@@ -27,5 +28,19 @@ public class TitleScreen extends World
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld(gameWorld);
         }
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Frog frog = new Frog();
+        addObject(frog,512,85);
+        Label label1 = new Label("Press <space> to start the game!", 40);
+        addObject(label1,getWidth()/2,245);
+        Label label2 = new Label("Use <-- and --> to move!", 40);
+        addObject(label2,getWidth()/2,306);
     }
 }
