10ce9f48d35b5f41223ee91c3ee3093df2300e67
anyarshah <348612037@gapps.yrdsb.ca>
2022-12-12 12:10:35 -0500

    Created a title screen

--- TitleScreen.java
+++ TitleScreen.java
@@ -1,35 +1,31 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Title Screen
+ * Creates a title screen
  * 
- * @author Anya Shah
- * @version December 9th, 2022
+ * @author Anya Shah 
+ * @version December 12th, 2022
  */
-public class Title extends World
+public class TitleScreen extends World
 {
-    Label titleLabel = new Label("Hungry Frog", 100);
+    Label titleLabel = new Label("Hungry Frog", 80);
     /**
-     * Constructor for objects of class Title.
+     * Constructor for objects of class TitleScreen.
      * 
      */
-    public Title()
+    public TitleScreen()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        
         addObject(titleLabel, getWidth()/2, getHeight()/2);
     }
-    /**
-     * The main world act loop
-     */
+    
     public void act()
     {
-        // Start the game if user presses the space bar
         if(Greenfoot.isKeyDown("space"))
         {
             MyWorld gameWorld = new MyWorld();
-            Greenfoot setWorld(World gameWorld);
+            Greenfoot.setWorld(gameWorld);
         }
     }
 }
